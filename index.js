bf0e3ab0600ed3a13183a60d8b8994a21d3118f7
Jack <96611933+JackTDU@users.noreply.github.com>
2022-05-30 18:16:28 +0700

    master(init): Done

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+import aizaWorldSmAtifacts from './sm-contracts/AizaWorldToken.json'
+import aizaFaucetSmAtifacts from './sm-contracts/FaucetToken.json'
+
 import characterSmAtifacts from './sm-contracts/Characters'
 import inoSmAtifacts from './sm-contracts/INO'
 import nftMarketSmAtifacts from './sm-contracts/NFTMarket'
 import priceOracleSmAtifacts from './sm-contracts/PriceOracle'
 import supplyBoxSmAtifacts from './sm-contracts/SupplyBoxs'
+
+const aizaWorldSmABI = aizaWorldSmAtifacts.abi
+const aizaWorldSmAdresses = aizaWorldSmAtifacts.networks
+
+const aizaFaucetSmABI = aizaFaucetSmAtifacts.abi
+const aizaFaucetSmAdresses = aizaFaucetSmAtifacts.networks
+
+const supplyBoxSmABI = supplyBoxSmAtifacts.abi
+const supplyBoxSmAdresses = supplyBoxSmAtifacts.networks
 
 const characterSmABI = characterSmAtifacts.abi
 const characterSmAdresses = characterSmAtifacts.networks
@@ -15,10 +27,12 @@ const nftMarketSmAdresses = nftMarketSmAtifacts.networks
 
 const priceOracleSmABI = priceOracleSmAtifacts.abi
 const priceOracleSmAdresses = priceOracleSmAtifacts.networks
-const supplyBoxSmABI = supplyBoxSmAtifacts.abi
-const supplyBoxSmAdresses = supplyBoxSmAtifacts.networks
 
 export {
+  aizaWorldSmABI,
+  aizaWorldSmAdresses,
+  aizaFaucetSmABI,
+  aizaFaucetSmAdresses,
   characterSmABI,
   characterSmAdresses,
   inoSmABI,
